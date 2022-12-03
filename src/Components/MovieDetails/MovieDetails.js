50328468be88b753ce26f2edaabfb4c75e54b74c
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-12-03 11:19:57 -0700

    Utilize SingleMovie data in the MovieDetails component so it displays title, poster, rating, etc.

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -3,7 +3,23 @@ import './MovieDetails.css'
 
 const MovieDetails = ({ singleMovie }) => {
     return (
-        <h2>movie details</h2>
+        <div>
+            <div>
+                <img className='poster-mini' src={singleMovie.movie.poster_path}/>
+            </div>
+            <div>
+                <h1>{singleMovie.movie.title}</h1>
+                <h3>{singleMovie.movie.tagline}</h3>
+                <h3>Genre: {singleMovie.movie.genres}</h3>
+                <h3>Rating: {singleMovie.movie.average_rating}/10</h3>
+                <p>{singleMovie.movie.overview}</p>
+                <h4>{singleMovie.movie.release_date}</h4>
+                <h4>Runtime: {singleMovie.movie.runtime} min</h4>
+                <h4>Budget: ${singleMovie.movie.budget}</h4>
+                <h4>Revenue: ${singleMovie.movie.revenue}</h4>
+            </div>
+            <button>Home</button>
+        </div>
     )
 }
 
