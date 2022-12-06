f16143eaa568ea909b8065c671903a2841346c9d
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-12-05 19:39:22 -0700

    Finish the testing of displaying movie details; after user clicks on first movie poster, it displays title, overview and mini poster

--- cypress/e2e/movie_detail_spec.cy.js
+++ cypress/e2e/movie_detail_spec.cy.js
@@ -15,5 +15,7 @@ describe('Movie Detail Interaction Flows', () => {
             fixture: '../fixtures/single_movie.json'
         })
             .get('.poster-mini').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg')
+            .get('.movieInfo').should('contain', 'Money Plane')
+            .and('contain', 'A professional thief with $40 million in debt and his family\'s life on the line must commit one final heist - rob a futuristic airborne casino filled with the world\'s most dangerous criminals.')
     })
 })
