f74cd13be469be32a751a0d255c476b244a9366c
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-12-07 18:39:36 -0700

    Add test to show user error message if site fails to load single movie detail after user clicks on movie poster (sad path)

--- cypress/e2e/server_failure.cy.js
+++ cypress/e2e/server_failure.cy.js
@@ -4,7 +4,24 @@ describe('Server Failure Page Flows', () => {
             method: 'GET',
             fixture: ''
         })
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000')
+        cy.get('h1').contains('Tiny Tomatoes')
+            .get('.logo').should('be.visible')
+            .get('[alt="cherry tomatoes on vine"]').should('be.visible')
+        cy.get('h2').contains('Oops, something went wrong. Please try again later.')
+    })
+
+    it('Should see error message if single movie details fail to load after selecting a movie', () => {
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+            method: 'GET',
+            fixture: '../fixtures/movies.json'
+        })
+        cy.visit('http://localhost:3000')
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
+            method: 'GET',
+            fixture: ''
+        })
+        cy.get('#694919').click()
         cy.get('h1').contains('Tiny Tomatoes')
             .get('.logo').should('be.visible')
             .get('[alt="cherry tomatoes on vine"]').should('be.visible')
