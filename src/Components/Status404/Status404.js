5bb7cac8e6105950324035f9655e72efcd63cd23
Jenn Yacoubian <jyacoubian@gmail.com>
2022-12-10 20:03:29 -0700

    Add button to navigate back to home for bad URL

--- src/Components/Status404/Status404.js
+++ src/Components/Status404/Status404.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Status404.css'
+import {Link} from 'react-router-dom'
 
 const Status404 = () => {
   return (
-    <h2>Oops, URL not found. Please try again.</h2>
+    <div className='redirect-container'>
+      <h2>Oops, URL not found. Please try again.</h2>
+      <Link to='/'>
+        <button className="redirect-button">Go Back Home</button>
+      </Link>
+    </div>
   )
 }
 
